801cb5fd53f9f773022a4b362340f5ad983403e8
luisagnes <lixilipe@gmail.com>
2023-06-26 08:41:22 -0300

    1. Criação Menus #5

--- control-finance/source/Components/CategoryCard/CategoryCard.jsx
+++ control-finance/source/Components/CategoryCard/CategoryCard.jsx
@@ -1,17 +1,21 @@
-import useGetCurrency from '@/source/hooks/useGetCurrency';
+import useGetCurrency from '../../hooks/useGetCurrency';
 import styles from './CategoryCard.module.scss';
 import {TbPencil} from 'react-icons/tb'
+import CategoryMenu from './CategoryMenu';
+import useMenu from '@/source/hooks/useMenu';
 
 const CategoryCard = ({ data }) => {
 
     const {title, amount, percentage} = data;
+    const [isVisible, toggleMenuHandler] = useMenu();
 
     const formatedAmount = useGetCurrency(amount);
 
     return(
         <div className={styles.card}>
-            <div className={styles.edit}>
-                <TbPencil className='icon hover'/>
+            <div className={styles.edit} >
+                <TbPencil className='icon hover'onClick={toggleMenuHandler} />
+                {isVisible && <CategoryMenu toggleMenuHandler={toggleMenuHandler}/>}
             </div>
             <h2>{title}</h2>
             <h3>{formatedAmount}</h3>
